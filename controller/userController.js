62c4f4de55e6229045157b0a47198f4be34de0e9
sandipbibe9912 <sbibe01@gmail.com>
2024-06-23 20:01:18 +0530

    'HII

--- controller/userController.js
+++ controller/userController.js
@@ -4,7 +4,8 @@ import userModel from "../models/userModel.js"
 export const createUser = async (request, response) => {
     try {
         const { name, contact, email, message } = request.body;
-
+      
+        const fname = name.split(" ")[0]
        
         if (!name || !contact) {
             return response.status(400).json({ error: 'Name or contact not provided' });
@@ -18,11 +19,14 @@ export const createUser = async (request, response) => {
             message
         });
 
+        
+
+       
       
         const result = await newUser.save();
 
        
-        return response.status(201).json({ msg: 'Response sent successfully!', success: true, result });
+        return response.status(201).json({ msg: `Hey ${fname} , thanks for your interest , i will get to you shortly`, success: true, result });
     } catch (error) {
        
         return response.status(500).json({ error: error.message });
